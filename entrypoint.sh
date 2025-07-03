2f8deed2a5fbb909a516dee02554c55f7bf894e3
Rana Salem <127218837+rtasalem@users.noreply.github.com>
2025-07-03 12:32:23 +0000

    MPDP-722 Restore local setup (#4)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+set -x
 
 echo "run_id: $RUN_ID in $ENVIRONMENT"
 
 NOW=$(date +"%Y%m%d-%H%M%S")
 
 if [ -z "${JM_HOME}" ]; then
-   JM_HOME=/opt/perftest
+  JM_HOME=/opt/perftest
 fi
 
 JM_SCENARIOS=${JM_HOME}/scenarios
@@ -18,18 +19,26 @@ SCENARIOFILE=${JM_SCENARIOS}/${TEST_SCENARIO}.jmx
 REPORTFILE=${NOW}-perftest-${TEST_SCENARIO}-report.csv
 LOGFILE=${JM_LOGS}/perftest-${TEST_SCENARIO}.log
 
+DOMAIN=${DOMAIN:-fcp-mpdp-frontend.${ENVIRONMENT}.cdp-int.defra.cloud}
+PORT=${PORT:-443}
+PROTOCOL=${PROTOCOL:-https}
+
 # Run the test suite
-jmeter -n -t ${SCENARIOFILE} -e -l "${REPORTFILE}" -o ${JM_REPORTS} -j ${LOGFILE} -f -Jenv="${ENVIRONMENT}"
+jmeter -n -t ${SCENARIOFILE} -e -l "${REPORTFILE}" -o ${JM_REPORTS} -j ${LOGFILE} -f \
+-Jenv="${ENVIRONMENT}" \
+-Jdomain="${DOMAIN}" \
+-Jport="${PORT}" \
+-Jprotocol="${PROTOCOL}"
 test_exit_code=$?
 
 # Publish the results into S3 so they can be displayed in the CDP Portal
 if [ -n "$RESULTS_OUTPUT_S3_PATH" ]; then
-   # Copy the CSV report file and the generated report files to the S3 bucket
+  # Copy the CSV report file and the generated report files to the S3 bucket
    if [ -f "$JM_REPORTS/index.html" ]; then
       aws --endpoint-url=$S3_ENDPOINT s3 cp "$REPORTFILE" "$RESULTS_OUTPUT_S3_PATH/$REPORTFILE"
       aws --endpoint-url=$S3_ENDPOINT s3 cp "$JM_REPORTS" "$RESULTS_OUTPUT_S3_PATH" --recursive
       if [ $? -eq 0 ]; then
-         echo "CSV report file and test results published to $RESULTS_OUTPUT_S3_PATH"
+        echo "CSV report file and test results published to $RESULTS_OUTPUT_S3_PATH"
       fi
    else
       echo "$JM_REPORTS/index.html is not found"
