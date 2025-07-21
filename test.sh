09b2fb147a146ddf0f60ccf0fc76ea9a54932ec7
Rana Salem <ranatasalem@gmail.com>
2025-07-21 15:45:03 +0100

    use perf test localstack instance of frontend one

--- test.sh
+++ test.sh
@@ -8,12 +8,12 @@ echo "Starting fcp-mpdp-performance-test-suite Docker container..."
 docker compose up --build -d 
 
 echo "Creating S3 bucket: $BUCKET_NAME..."
-docker compose exec development aws --endpoint-url=http://fcp-mpdp-frontend-localstack:4566 s3 mb "s3://$BUCKET_NAME" 
+docker compose exec development aws --endpoint-url=http://fcp-mpdp-performance-test-suite-localstack:4566 s3 mb "s3://$BUCKET_NAME" 
 echo "S3 bucket: $BUCKET_NAME successfully created"
 
 echo "Waiting for S3 bucket $BUCKET_NAME to become accessible..."
 for i in {1..10}; do
-  if docker compose exec development aws s3 ls "s3://$BUCKET_NAME" --endpoint-url=http://fcp-mpdp-frontend-localstack:4566 > /dev/null 2>&1; then
+  if docker compose exec development aws s3 ls "s3://$BUCKET_NAME" --endpoint-url=http://fcp-mpdp-performance-test-suite-localstack:4566 > /dev/null 2>&1; then
     echo "S3 bucket fcp-mpdp-performance-test-suite is now accessible"
     break
   fi
@@ -30,6 +30,6 @@ rm -rf "${LOCAL_REPORTS_DIR:?}"/*
 mkdir -p "$LOCAL_REPORTS_DIR"
 
 echo "Downloading reports from S3 bucket $BUCKET_NAME..."
-docker compose exec development aws s3 cp "s3://$BUCKET_NAME/" "/opt/perftest/reports" --recursive --endpoint-url=http://fcp-mpdp-frontend-localstack:4566
+docker compose exec development aws s3 cp "s3://$BUCKET_NAME/" "/opt/perftest/reports" --recursive --endpoint-url=http://fcp-mpdp-performance-test-suite-localstack:4566
 
 echo "Reports have been downloaded successfully"
